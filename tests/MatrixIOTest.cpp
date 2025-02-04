f37e8c32aed93d55e8b56a092991195a08b6a153
Vaish-W <wanivaishnavi16@gmail.com>
2025-02-04 17:05:54 +0100

    Fix style_check

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -1,8 +1,8 @@
 #define BOOST_TEST_DYN_LINK
 #include <Eigen/Dense>
 #include <boost/test/unit_test.hpp>
-#include "matrixIO.hpp"
 #include <sstream>
+#include "matrixIO.hpp"
 
 using namespace Eigen;
 
